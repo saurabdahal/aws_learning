580aae69daba6fd48e4f1e2a6c729b4373d5fb3b
Saurab Dahal <saurab@Saurav.localdomain>
2025-07-04 14:06:22 -0400

    added comment for clarity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from s3.BucketManager import BucketManager
 
 if __name__ == '__main__':
-    BucketManager().list_buckets()
+    BucketManager().create_bucket(region="us-east-2")
 
--- s3/BucketManager.py
+++ s3/BucketManager.py
@@ -1,22 +1,26 @@
 import boto3
+from botocore.exceptions import ClientError
 
 
 class BucketManager:
     def __init__(self):
+        # please keep in mind following things while choosing a bucket name
+        # 1. Bucket names must be 3 to 63 characters and unique within the global namespace
+        # 2. Bucket names must also begin and end with a letter or number and must be lowercase
+        # 3. Valid characters are a-z, 0-9, periods (.), and hyphens (-)
         self.bucket_name = "aws-demo-fruit"  # name must be unique globally
         self.s3 = boto3.client('s3')
 
-    def create_bucket(self, bucket_name=None):
+    def create_bucket(self, region, bucket_name=None):
         try:
             if not bucket_name:
                 bucket_name = self.bucket_name
-            print(bucket_name)
-            self.s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': 'us-east-2'})
+            self.s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': region})
             # you can create bucket in any region you like
-        except Exception as e:
-            print(f"Error: {e.args}")
+        except ClientError as e:
+            print(f"Error: {e.response['Error']['Message']}")
 
-    def list_buckets(self):
+    def list_all_buckets(self):
         try:
             resp = self.s3.list_buckets()
             buckets = resp.get('Buckets', [])
@@ -25,8 +29,8 @@ class BucketManager:
                     print(b)
             else:
                 print("no buckets found")
-        except Exception as e:
-            print(f"Error: {e.args}")
+        except ClientError as e:
+            print(f"Error: {e.response['Error']['Message']}")
 
 
 
